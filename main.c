4e2d7445f8608ab48a1bf51f975553441273edc8
bariscode29 <76122901+bariscode29@users.noreply.github.com>
2021-01-22 12:28:45 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int integer = 5;
+    float asd = 5.5;
+    char characterim = 'A';
+    char characterim2 = 65;
+    double dble = 197.19273292928272892;
+    short a = 2;
+    long b = 923597262;
+    
+    printf("%d %f %c %c %f %d %ld", integer, asd, characterim, characterim2, dble, a, b);
+    
+    return 0;
+}
